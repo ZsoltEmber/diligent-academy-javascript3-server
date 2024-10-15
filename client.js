72144dd3c90ab1eeade162fcd716ed2e88613654
ZsoltEmber <132936073+ZsoltEmber@users.noreply.github.com>
2024-10-15 11:42:47 +0200

    implement validate fn to add param

--- client.js
+++ client.js
@@ -1,3 +1,38 @@
+import {getTodos, getTodo, addTodo, modifyTodo, deleteTodo  } from './todo.js';
+
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
+const action = prompt('What would you like to do? (list, get, add, update, delete): ');
+function todoApp(){
+    switch (action) {
+        case "list":
+            getTodos();
+            break;
+
+        case "get":
+            const idToGet = prompt('Enter the id of the todo you want to read: ');
+            getTodo(idToGet);
+            break;
+
+        case "add":
+            const todoName = prompt('Enter the name of the todo you want to add: ');
+            addTodo(todoName);
+            break;
+
+        case "update":
+            const idToUpdate = prompt('Enter the id of the todo you want to update: ');
+            modifyTodo(idToUpdate);
+            break;
+    
+        case "delete":
+            const idToDelete = prompt('Enter the id of the todo you want to delete: ');
+            deleteTodo(idToDelete);
+            break;
+    
+        default:
+            break;
+    }
+}
+
+todoApp();
--- validate.js
+++ validate.js
@@ -1,12 +1,12 @@
 import { readFileSync } from "fs";
 
 export function validateCommand(command) {
-    const commands = ["get", "add", "update", "delete"]
+    const commands = ["get", "list", "add", "update", "delete"]
     if (commands.includes(command)) {
         return command;
     }
     else {
-        throw new Error("Parameter is not a command, try: 'get', 'add', 'update' or 'delete'")
+        throw new Error("Parameter is not a command, try: 'list', 'get', add', 'update' or 'delete'")
     }
 }
 
@@ -24,6 +24,11 @@ export function validateId(id) {
     }else{
         throw new Error(`No todo found with ID: ${id}`)
     }
+}
 
-
+export function validateAddParam(name){
+      if (typeof name !== 'string' || name?.length === 0) {
+        throw new Error('The title must be a non zero length string.')
+      }
+      return name;
 }
